129b0ad5a101b4cb33f854123a3804fccb30ef86
MatteoIovino <matteoi@kth.se>
2021-02-23 17:17:17 +0100

    Created skeleton for test routines.

---
+++ metalhistory/tests/test_query_api.py
@@ -0,0 +1,39 @@
+"""
+Test routines for the LastFM class
+"""
+
+from metalhistory.data_query_functions import LastFM
+
+def test_authenticate():
+    """
+    Test the authenticate_from_dotenv function
+    """
+
+
+def test_build_request():
+    """
+    Test the build_request function
+    """
+
+
+def test_clean_string():
+    """
+    Test the clean_string function
+    """
+
+
+def test_get_album_matches():
+    """
+    Test the get_album_matches function
+    """
+
+
+def test_get_album_info():
+    """
+    Test the get_album_info function
+    """
+
+def test_gget_track_info():
+    """
+    Test the get_track_info function
+    """
