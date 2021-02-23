bad3bbf66679d4f6985a958c029610e87dcc6033
MatteoIovino <matteoi@kth.se>
2021-02-23 17:43:36 +0100

    Added test routine for authenticate function.

--- metalhistory/tests/test_query_api.py
+++ metalhistory/tests/test_query_api.py
@@ -6,8 +6,14 @@ from metalhistory.data_query_functions import LastFM
 
 def test_authenticate():
     """
-    Test the authenticate_from_dotenv function
+    Test the authenticate_from_dotenv function.
+    In the .env file there is a value for ORACLE_KEY for testing purposes.
     """
+    oracle_key = 'thisIsTheOracleKey123'
+    lastFM_obj = LastFM()
+    api_key = lastFM_obj.authenticate_from_dotenv('ORACLE_KEY')
+
+    assert oracle_key == api_key
 
 
 def test_build_request():
