7bc2077d06f538d705b8a564a2edc71dbbd94b0b
matteoi <matteoi@kth.se>
2021-02-26 10:24:57 +0100

    feat: Added test skeleton for the visualization api.

---
+++ metalhistory/tests/test_visualization_api.py
@@ -0,0 +1,22 @@
+"""
+Test routines for the data visualization class
+"""
+
+from metalhistory.visualization_api import Visualize
+
+def test_artist_cloud(self, threshold=20, path='./'):
+        """
+        Test the artist word cloud.
+        """
+
+
+def test_album_cloud(self, threshold=20, path='./'):
+     """
+        Test the album word cloud.
+        """
+
+
+def test_genre_cloud(self, threshold=20, path='./'):
+     """
+        Test the genres word cloud.
+        """
