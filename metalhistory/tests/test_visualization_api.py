180574e68ecdfc8f2354711c3bc8bfe77955d599
matteoi <matteoi@kth.se>
2021-02-26 12:35:57 +0100

    feature: Added a test function.

--- metalhistory/tests/test_visualization_api.py
+++ metalhistory/tests/test_visualization_api.py
@@ -4,19 +4,39 @@ Test routines for the data visualization class
 
 from metalhistory.visualization_api import Visualize
 
-def test_artist_cloud(self, threshold=20, path='./'):
-        """
-        Test the artist word cloud.
-        """
+import os
+import sys
+import pandas as pd
 
 
-def test_album_cloud(self, threshold=20, path='./'):
+# get path of the dataset
+script_dir = os.path.dirname(__file__)
+parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+data_path = os.path.join(parent_dir, '/data/MA_10k_albums.csv')
+
+def test_load_dataset():
+    """
+    Test the load function.
+    """
+    vis = Visualize(data_path)
+    df = vis.load_dataframe()
+
+    df.head()
+
+
+def test_artist_cloud(threshold=20, path='./'):
+    """
+    Test the artist word cloud.
+    """
+
+
+def test_album_cloud(threshold=20, path='./'):
      """
-        Test the album word cloud.
-        """
+    Test the album word cloud.
+    """
 
 
-def test_genre_cloud(self, threshold=20, path='./'):
+def test_genre_cloud(threshold=20, path='./'):
      """
-        Test the genres word cloud.
-        """
+    Test the genres word cloud.
+    """
