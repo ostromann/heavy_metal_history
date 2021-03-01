00704228d6e0ba7425cda900f0db754a09912d7a
MatteoIovino <matteoi@kth.se>
2021-03-01 11:46:16 +0100

    feat: Added function genereting the artists word cloud.

--- metalhistory/visualization_api.py
+++ metalhistory/visualization_api.py
@@ -57,7 +57,7 @@ class Visualize():
         return df
 
 
-    def artist_barplot(self, n_albums=20, path='./artist_average.png'):
+    def artist_barplot(self, n_albums=20, path='artist_bar.svg'):
         """
         Visualize a histogram plot with artist names.
         Artist are scored based on the average of the MA score of each album
@@ -72,6 +72,46 @@ class Visualize():
         plt.title("Best artist with at least " + str(n_albums) + " albums.")
         plt.xlabel("")
         plt.ylabel("Average MA score")
+        plt.savefig(path)
+
+
+    def artist_cloud(self, words_limit=20, min_albums=15, path='artist_cloud.svg'):
+        #TODO: implement this with a real dataset
+        """
+        Visualize a world cloud with artist names.
+        The artist names correspond to the most influential ones, from 1 to 'ords_limits'.
+        The atists are selected such that they published at least 'min_albums' albums.
+        The figure will be saved in the specified path.
+        """
+
+        df = self.prune_N(min_albums)
+        artist_df = df.groupby('artist')
+
+        artist_df = artist_df.count().sort_values(by='MA_score', ascending=False)["MA_score"]
+        index_obj = artist_df.index
+        index_list = []
+
+        txt_path = 'artist_cloud.txt'
+        # cancel the file if it exists
+        out_file = open(txt_path, 'w')
+        out_file.write("")
+        
+        for name in index_obj:
+            # replace space with tabs so that artists names with multiple words are counted as a single entity
+            index_list.append(name.replace(' ', '_'))
+            count = artist_df.loc[name]
+            out_file = open(txt_path, 'a')
+            for i in range(count):
+                out_file.write(index_list[-1] + " ")
+
+        # create and generate a word cloud image:
+        out_file = open(txt_path, 'r')
+        contents = out_file.read()
+        wordcloud = WordCloud(collocations=False, max_words=words_limit).generate(contents)
+
+        # Display the generated image:
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
         plt.savefig(path)
 
 
