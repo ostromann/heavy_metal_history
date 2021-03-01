917a32cd50fb204f07e25840927c921b89199698
Arvi Jonnarth <arvi.jonnarth@liu.se>
2021-03-01 11:05:31 +0100

    test: add tests for the build_request function

--- metalhistory/tests/test_query_api.py
+++ metalhistory/tests/test_query_api.py
@@ -2,7 +2,21 @@
 Test routines for the LastFM class
 """
 
+import os
+from dotenv import load_dotenv
 from metalhistory.data_query_functions import LastFM
+
+BASE_STR = 'http://ws.audioscrobbler.com/2.0/?'
+
+
+def get_api_key():
+    """
+    Retrieve the API key without using the LastFM class.
+    """
+    env_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir, '.env'))
+    load_dotenv(dotenv_path=env_path)
+    return os.getenv('LASTFM_API_KEY')
+
 
 def test_authenticate():
     """
@@ -16,10 +30,42 @@ def test_authenticate():
     assert oracle_key == api_key
 
 
-def test_build_request():
+def test_build_request_dummy_input():
     """
-    Test the build_request function
+    Test the build_request function with dummy inputs.
     """
+    lastFM_obj = LastFM()
+    api_key = get_api_key()
+    kwargs = {'method': 'dummyMethod',
+              'format_spec': 'json',
+              'dummyArg1': 'arg1',
+              'dummyArg2': 'arg2'}
+    request_str = lastFM_obj.build_request(**kwargs)
+    correct_str = BASE_STR + '&api_key=' + api_key + \
+        '&method=dummyMethod' + \
+        '&dummyArg1=arg1' + \
+        '&dummyArg2=arg2' + \
+        '&format=json'
+    assert request_str == correct_str
+
+
+def test_build_request_non_clean_input():
+    """
+    Test the build_request function with inputs that should be formated before
+    returning the final url.
+    """
+    lastFM_obj = LastFM()
+    api_key = get_api_key()
+    kwargs = {'method': 'album.getinfo',
+              'format_spec': 'incorrect_format',
+              'artist': 'Black Sabbath',
+              'album': '  Paranoid'}
+    request_str = lastFM_obj.build_request(**kwargs)
+    correct_str = BASE_STR + '&api_key=' + api_key + \
+        '&method=album.getinfo' + \
+        '&artist=Black%20Sabbath' + \
+        '&album=Paranoid'
+    assert request_str == correct_str
 
 
 def test_clean_string():
