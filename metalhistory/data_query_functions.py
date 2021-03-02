2123b4dc4b2dc39f975821e764292e31cbc3c6a4
Ostromann <oliver.stromann@scania.com>
2021-03-02 15:40:08 +0100

    fix: Removed unnecessary print output.

--- metalhistory/data_query_functions.py
+++ metalhistory/data_query_functions.py
@@ -350,8 +350,7 @@ class LastFM():
 
         """
         r_dict = {}
-        for field in fields:
-            print(field)   
+        for field in fields:   
             if field == 'release-date':
                 r_dict[field] = self.get_release_date(json['mbid'])
             elif field == 'tags':
