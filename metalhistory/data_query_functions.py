15dc2df33c87dc1f39a6584ca17fcf26260fdf72
Ostromann <oliver.stromann@scania.com>
2021-03-03 17:00:56 +0100

    fix: Check for string and use lower case tags

--- metalhistory/data_query_functions.py
+++ metalhistory/data_query_functions.py
@@ -301,10 +301,11 @@ class LastFM():
         tag_list = []
         ignored_tag_list = []
         for tag in tags['tag']:
-            if tag['name'] in self.config['user settings']['accepted tags']:
-                tag_list.append(tag['name'])
+            if type(tag['name']==str:
+                if tag['name'].lower in self.config['user settings']['accepted tags']:
+                    tag_list.append(tag['name'].lower)
             else:
-                ignored_tag_list.append(tag['name'])
+                ignored_tag_list.append(tag['name'].lower)
         return tag_list, ignored_tag_list
 
     def get_release_date(self, mbid):
