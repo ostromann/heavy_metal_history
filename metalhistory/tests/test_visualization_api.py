9705af6b704d50a0de27b7028a375b4a614ffbc3
MatteoIovino <matteoi@kth.se>
2021-03-03 14:57:18 +0100

    feat: Added test function.

--- metalhistory/tests/test_visualization_api.py
+++ metalhistory/tests/test_visualization_api.py
@@ -74,6 +74,33 @@ def test_artist_barplot():
     assert oracle_isthere == test_file.is_file()
 
 
+def test_generate_txt():
+    """
+    Test function for the generate txt from df function.
+    """
+
+    vis = Visualize(data_path)
+    
+    # the oracle know that Iron Maiden has published 38 albums
+    # so we create a txt file just with Iron Maiden entry which should be 'Iron_Maiden' repeated 38 times.
+    # The first 11 caracters should also be 'Iron_Maiden'
+    oracle_albums = 38
+    oracle_isthere = True
+    oracle_string = 'Iron_Maiden'
+
+    artist_df = vis.prune_and_group(oracle_albums)
+    artist_df = artist_df.count().sort_values(by='MA_score', ascending=False)["MA_score"]
+    test_path = vis.generate_text_from_df(artist_df, file_name='/test_txt.txt')
+
+    test_file = Path(test_path)
+
+    out_file = open(test_path, 'r')
+    first_artist = out_file.read(11)
+
+    assert oracle_isthere == test_file.is_file()
+    assert oracle_string == first_artist
+
+
 def test_artist_quantity():
     """
     Test the artist quantity cloud function.
